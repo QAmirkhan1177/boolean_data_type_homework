646707d5254b1766132eae9cc1fd2aa2586e0c5d
QAmirkhan1177 <amirxonqarshiyev@gmail.com>
2023-09-23 16:57:56 +0500

    1 tayyor

--- bool01.py
+++ bool01.py
@@ -1,11 +1,14 @@
-# def main(a,b):
-#     """
-#     Check the following statement "they are equal"
-#     Args:
-#         a: int
-#         b: int
-#     Returns:
-#         bool
-#     """
-    # Write your code here 
-print(5==5) 
+def main(a,b):
+    """
+    Check the following statement "they are equal"
+    Args:
+        a: int
+        b: int
+    Returns:
+        bool
+    """
+    return a==b
+a=5
+b=5
+print(main(a,b))
+ 
